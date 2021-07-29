2445f28153c08706fadc92fec04b4f6bcf92c622
beano38 <benelder38@gmail.com>
2021-07-29 15:47:32 -0400

    Update scripts

--- python/sample_webex_post.py
+++ python/sample_webex_post.py
@@ -2,7 +2,7 @@ import requests
 
 URL = "https://webexapis.com/v1/messages"
 ROOM_ID = "5453daf0-e578-11eb-b31c-8f9d0ae3efb6"
-TOKEN = "OWIyMjEyMjgtMDk5OS00MTE5LWEyNjgtZDUyNjc5NzZmYjQwNWVlNTZmNzctYzYx_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
+TOKEN = "token"
 
 
 payload = {
